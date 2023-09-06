fc207213768de4ef274527a710a8d1694dd45adb
Michael Tanaka <tanakam4214@gmail.com>
2023-09-06 00:00:18 -0700

    Lab Completed

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,58 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at).all()
 
-@app.route('/messages/<int:id>')
+    if request.method == "GET":
+        serialized_messages = [message.to_dict() for message in messages]
+
+        response = make_response(jsonify(serialized_messages), 200)
+
+        return response
+    elif request.method == "POST":
+        new_message = Message(
+            body=request.json.get("body"),
+            username=request.json.get("username"),
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+        response = make_response(jsonify(new_message_dict), 201)
+        return response
+
+
+@app.route("/messages/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
 
-if __name__ == '__main__':
+    if request.method == "GET":
+        message_dict = message.to_dict()
+        response = make_response(message_dict, 200)
+        return response
+    elif request.method == "PATCH":
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+            response = make_response(message_dict, 200)
+            return response
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {
+            "message": "This message has been deleted",
+            "delete_successful": True,
+        }
+        response = response_body(jsonify(response_body), 200)
+        return response
+
+
+if __name__ == "__main__":
     app.run(port=5555)
